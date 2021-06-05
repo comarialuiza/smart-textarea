661cb5155e711e981b534a672b0d39192058f01e
Maria Luiza <co.maria.luiza@gmail.com>
2021-06-04 23:34:47 -0300

    creates basic logic

--- src/components/Textarea.tsx
+++ src/components/Textarea.tsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Textarea = () => {
+    const [content, setContent] = useState('');
+    const [maxNumOfCharacters, setMaxNumOfCharacters] = useState(40);
+    const [availableNumOfCharacters, setAvabilableNumOfCharacters] = useState(maxNumOfCharacters);
+    const [availablePercentageOfCharacters, setAvailablePercentageOfCharacters] = useState(100);
+
+    const handleValueChange = (e: React.FormEvent<HTMLTextAreaElement>) => setContent(e.currentTarget.value);
+
+    useEffect(() => {
+        const availableNum = maxNumOfCharacters - content.length;
+        const availablePercentage = Math.round(100 - ((availableNum / maxNumOfCharacters) * 100));
+
+        setAvabilableNumOfCharacters(availableNum);
+
+        if (content.length > maxNumOfCharacters) {
+            setAvailablePercentageOfCharacters(100);
+        } else {
+            setAvailablePercentageOfCharacters(availablePercentage);
+        }
+    }, [content, maxNumOfCharacters]);
+
     return (
-        <textarea></textarea>
+        <>
+            <textarea
+                value={ content }
+                onChange={ handleValueChange }
+            />
+            <span>{ availablePercentageOfCharacters }%</span>
+            <span>{ availableNumOfCharacters }</span>
+            <button>Submit</button>
+        </>
+
     );
 };
 
